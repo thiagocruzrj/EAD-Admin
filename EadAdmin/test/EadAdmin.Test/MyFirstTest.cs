55a16dbc53309be98cea70e6e94cfa049a49b294
Thiago Cruz <thagocruz@gmail.com>
2020-07-10 19:33:05 -0300

    Create MyFirstTest.cs

---
+++ EadAdmin/test/EadAdmin.Test/MyFirstTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace EadAdmin.Domain
+{
+    public class MyFirstTest
+    {
+
+        [Fact(DisplayName = "Variable 1 should equals variable 2")]
+        [Trait("First Test values", "Testing values first time")]
+        public void RecebeTwoVariables_IfNotEquals_ReturnTrue()
+        {
+            var variable1 = 1;
+            var variable2 = 2;
+
+            Assert.NotEqual(variable1, variable2);
+        }
+    }
+}
