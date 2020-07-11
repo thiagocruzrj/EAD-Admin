31bb26efcca2b151f0e1191b9c06946d25e2d9e5
Thiago Cruz <thagocruz@gmail.com>
2020-07-11 15:06:21 -0300

    Move some classes and add new base course test

---
+++ EadAdmin/test/EadAdmin.Test/Course/CourseTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace EadAdmin.Domain.Course
+{
+    public class CourseTest
+    {
+        private string name = "C#";
+        private double workLoad = 55.5;
+        private string tagetAudience = "Students";
+        private double price = 40.5;
+
+        [Fact]
+        public void ShouldCreateCourse()
+        {
+            var course = new Course(name, workLoad, tagetAudience, price);
+            Assert.Empty(name, course.Name);
+            Assert.Empty(workLoad, course.WorkLoad);
+            Assert.Empty(tagetAudience, course.TagetAudience);
+            Assert.Empty(price, course.Price);
+        }
+    }
+    public class Course
+    {
+        public Course(string name, double workLoad, string tagetAudience, double price)
+        {
+
+        }
+    }
+}
