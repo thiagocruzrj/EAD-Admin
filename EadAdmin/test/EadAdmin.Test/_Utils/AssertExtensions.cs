c13490485a2f89371f6b2abea29ab2be589cd50d
Thiago Cruz <thagocruz@gmail.com>
2020-07-11 19:53:32 -0300

    Create AssertExtensions.cs

---
+++ EadAdmin/test/EadAdmin.Test/_Utils/AssertExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace EadAdmin.Domain._Utils
+{
+    public static class AssertExtensions
+    {
+        public static void WithMessage(this ArgumentException exception, string message)
+        {
+            if (exception.Message == message)
+                Assert.True(true);
+            else
+                Assert.False(true);
+        }
+    }
+}
