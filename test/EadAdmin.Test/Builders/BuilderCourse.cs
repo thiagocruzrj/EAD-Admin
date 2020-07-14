e61dfd08af4fd521248f36ded064c093f8e45fa2
Thiago Cruz <thagocruz@gmail.com>
2020-07-13 22:32:18 -0300

    Add base builders properties

--- test/EadAdmin.Test/Builders/BuilderCourse.cs
+++ test/EadAdmin.Test/Builders/BuilderCourse.cs
@@ -5,15 +5,45 @@ namespace EadAdmin.Domain.Builders
 {
     public class BuilderCourse
     {
-        private readonly string _name = "C#";
-        private readonly string _description = "Description Test";
-        private readonly double _workLoad = 80;
-        private readonly TargetAudience _targetAudience = TargetAudience.Student;
-        private readonly double _price = 40;
+        private string _name = "C#";
+        private string _description = "Description Test";
+        private double _workLoad = 80;
+        private TargetAudience _targetAudience = TargetAudience.Student;
+        private double _price = 40;
 
         public static BuilderCourse New()
         {
             return new BuilderCourse();
+        }
+
+        public BuilderCourse WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public BuilderCourse WithDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+
+        public BuilderCourse WithWorkload(double workload)
+        {
+            _workLoad = workload;
+            return this;
+        }
+
+        public BuilderCourse WithPrice(double price)
+        {
+            _price = price;
+            return this;
+        }
+
+        public BuilderCourse WithTargetAudience(TargetAudience targetAudience)
+        {
+            _targetAudience = TargetAudience.Student;
+            return this;
         }
 
         public app.Course Build()
