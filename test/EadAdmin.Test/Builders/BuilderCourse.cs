4f05387d4ed4050f833c1240393055f78dc98396
Thiago Cruz <thagocruz@gmail.com>
2020-07-13 22:28:01 -0300

    Add new properties to generate a course

--- test/EadAdmin.Test/Builders/BuilderCourse.cs
+++ test/EadAdmin.Test/Builders/BuilderCourse.cs
@@ -1,11 +1,24 @@
-﻿namespace EadAdmin.Domain.Builders
+﻿using EadAdmin.Domain.Course;
+using app = EadAdmin.Domain.Course;
+
+namespace EadAdmin.Domain.Builders
 {
     public class BuilderCourse
     {
+        private readonly string _name = "C#";
+        private readonly string _description = "Description Test";
+        private readonly double _workLoad = 80;
+        private readonly TargetAudience _targetAudience = TargetAudience.Student;
+        private readonly double _price = 40;
 
         public static BuilderCourse New()
         {
             return new BuilderCourse();
+        }
+
+        public app.Course Build()
+        {
+            return new app.Course(_name, _description, _workLoad, _targetAudience, _price);
         }
     }
 }
