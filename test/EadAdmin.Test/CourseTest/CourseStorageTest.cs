c23db192ce8e72938ca418c22e83556a633b1df4
Thiago Cruz <thagocruz@gmail.com>
2020-07-18 18:42:52 -0300

    Red on course add

--- test/EadAdmin.Test/CourseTest/CourseStorageTest.cs
+++ test/EadAdmin.Test/CourseTest/CourseStorageTest.cs
@@ -1,6 +1,24 @@
-﻿namespace EadAdmin.DomainTest.CourseTest
+﻿using System;
+using Xunit;
+
+namespace EadAdmin.DomainTest.CourseTest
 {
     public class CourseStorageTest
     {
+        [Fact]
+        public void ShouldAddCourse()
+        {
+            var courseDto = new CourseDto
+            {
+                Name = "C#",
+                Description = "A CSharp´course",
+                WorkLoad = 50.5,
+                TargetAudienceId = 1,
+                Price = 30.5
+            };
+
+            var storageCourse = new StorageCourse();
+            storageCourse.Store(courseDto);
+        }
     }
 }
