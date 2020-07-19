2616d0c07bfcb6a31300bacd97b1da97b3e51729
Thiago Cruz <thagocruz@gmail.com>
2020-07-19 16:14:13 -0300

    Add new controller

---
+++ src/EadAdmin.Web/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+﻿using EadAdmin.Domain.Courses;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EadAdmin.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var courses = new List<CourseDto>();
+        }
+    }
+}
--- src/EadAdmin/EadAdmin.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EadAdmin.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
